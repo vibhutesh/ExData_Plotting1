e5c4ae9cb89a69a493e1054df5695e92e0be27ac
vibhutesh <vibhutesh@gmail.com>
2018-11-29 06:59:50 +0530

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -11,12 +11,12 @@ RangeData$Sub_metering_1<- as.numeric(RangeData$Sub_metering_1)
 RangeData$Sub_metering_2<- as.numeric(RangeData$Sub_metering_2)
 RangeData$Sub_metering_3<- as.numeric(RangeData$Sub_metering_3)
 
-
+png("plot3.png", width = 480, height = 480)
 with(RangeData, {
   plot(datetime,Sub_metering_1, type="l", ylab="Energy sub metering", xlab = "")
   lines(datetime,Sub_metering_2,col='Red')
   lines(datetime,Sub_metering_3,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1, lwd = 2)
-png("plot3.png", width = 480, height = 480)
+
 dev.off()
